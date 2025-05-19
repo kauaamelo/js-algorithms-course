11f6f5cd77bcea2fa3fc4795eec29ec5220a502b
kaua_amelo <kaua.amelo2020@gmail.com>
2025-05-19 01:16:24 -0300

    feat: exercicio classificar idade

--- activities-one.js
+++ activities-one.js
@@ -15,3 +15,28 @@ if(numero % 2 === 0) {
     console.log(`Numero ${numero} é impar!`)
 }
 
+/* 2. Crie um programa que classifica a idade de uma pessoa em categorias (criança,
+adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de
+controle if-else. */
+
+let crianca = 12;
+let adolescente = 17;
+let adulto = 18;
+let idoso = 60;
+let idadePessoa = 0;
+
+idadePessoa = Number(prompt(`Digite sua idade: `));
+
+if(idadePessoa <= crianca) {
+    console.log(`Você tem ${idadePessoa} anos, categoria: criança`);
+} else if (idadePessoa <= adolescente){
+    console.log(`Você tem ${idadePessoa} anos, categoria: adolescente`);
+} else if (idadePessoa >= adulto && idadePessoa < 60){
+    console.log(`Você tem ${idadePessoa} anos, categoria: adulto`);
+} else if (idadePessoa >= idoso){
+    console.log(`Você tem ${idadePessoa} anos, categoria: idoso`);
+} else {
+    if (idadePessoa !== NaN) {
+   console.log(`Erro: idade inválida`)
+}
+}

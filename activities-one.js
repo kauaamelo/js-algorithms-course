f6732c693b3f5efdd8ea1759183ada8a74561cdb
kaua_amelo <kaua.amelo2020@gmail.com>
2025-05-20 01:09:48 -0300

    feat: imprimir número digitado com loop

--- activities-one.js
+++ activities-one.js
@@ -185,3 +185,11 @@ for (let i = 10; i >= 1; i--) {
   console.log(i);
 }
     console.log("Fim");
+
+/* 10. Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes. */
+
+const numeroInteiro = parseInt(prompt("Digite um número inteiro: "));
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i} - ${numeroInteiro}`);
+}
